f4b3935b29258de6eca715eb1c522fadac252c42
Mirwais <mirwais.sanaie@gmail.com>
2024-11-15 15:14:05 +0430

    Ui completed ;

--- src/components/App.jsx
+++ src/components/App.jsx
@@ -1,7 +1,33 @@
-export default function App(){
-    return(
-        <div>
-            <h1 className="font-bold text-4xl bg-slate-500">Hello world</h1>
+import { useState } from "react";
+import "./App.css";
+
+export default function App() {
+  const [qoute, setQoute] = useState({
+    text: "The future belongs to those who believe in the beauty of their dreams.",
+    author: "Unknown",
+  });
+
+  return (
+    <div className="container">
+      <div className="qoute">{qoute.text}</div>
+      <div>
+        <div className="line"></div>
+        <div className="bottom">
+          <div className="author">{qoute.author}</div>
+          <div className="icons">
+            <img
+              style={{ width: "25px", height: "25px", cursor: "pointer" }}
+              src="./X_dark.svg"
+              alt=""
+            />
+            <img
+              style={{ width: "25px", height: "25px", cursor: "pointer" }}
+              src="./reload-50.png"
+              alt=""
+            />
+          </div>
         </div>
-    )
+      </div>
+    </div>
+  );
 }
